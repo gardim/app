e9d9c6ac7c4eae79f635467cb275ce33d4c7b9e0
Lavínia Beghini <lavinia.beghini@creditas.com>
2023-02-19 01:37:51 +0100

    Implements theming

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,9 +1,31 @@
 /// <reference types="cypress" />
 
+import { lightGreenColors, darkGreenColors } from '../../src/utils/colors';
+
 describe('template spec', () => {
 	it('works', () => {
 		cy.visit('/');
 		cy.contains('Open up App.js to start working on your app!')
 			.should('be.visible');
+	});
+
+	it('changes theme', () => {
+		cy.visit('/');
+		
+		cy.contains('Open up App.js to start working on your app!')
+			.should('be.visible')
+			.should('have.css', 'color', lightGreenColors.colors.onBackground);
+
+		cy.get('img[alt=""]').click();
+
+		cy.contains('Preferences')
+			.should('be.visible');
+
+		cy.contains('Dark Theme')
+			.should('be.visible')
+			.click();
+
+		cy.contains('Open up App.js to start working on your app!')
+			.should('have.css', 'color', darkGreenColors.colors.onBackground);
 	});
 });

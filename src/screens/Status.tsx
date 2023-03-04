aae0da6e0b022e34a7e9d22874c21e0513211151
Lavínia Beghini <lavinia.beghini@creditas.com>
2023-03-04 01:11:14 +0100

    Remove unused import

--- src/screens/Status.tsx
+++ src/screens/Status.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { View } from 'react-native';
 import { Text, Card, Avatar } from 'react-native-paper';
 import { LinearGaugeChart } from '../components/LinearGaugeChart';
